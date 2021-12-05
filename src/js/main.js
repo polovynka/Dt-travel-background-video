5fbeebed70a89355d9a75c1fc6300b783e1e96e2
Vladyslav Polovynka <polovynka.dev@gmail.com>
2021-12-05 15:47:31 +0200

    push files to github

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+// SideBarMenu
+const toggleIcon = document.querySelector('.toggle__icon');
+const sideBar = document.querySelector('.side-bar');
+
+
+toggleIcon.addEventListener('click', (event) => {
+	event.stopPropagation();
+	toggleIcon.classList.toggle('active');
+	sideBar.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+	const target = event.target;
+
+	const itsMenu = target == sideBar || sideBar.contains(target);
+	const itsBtnMenu = target == toggleIcon;
+	const menuIsActive = sideBar.classList.contains('active');
+
+	if (!itsMenu && !itsBtnMenu && menuIsActive) {
+		toggleIcon.classList.toggle('active');
+		sideBar.classList.toggle('active');
+	}
+});
+
+
